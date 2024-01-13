264a4759ac16912c3cd644b9dd7a0712d7a219c9
Jeremy <jeremypisces04@gmail.com>
2024-01-13 16:34:14 +0700

    First

---
+++ client-app/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme.js";
+import "./index.css";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);

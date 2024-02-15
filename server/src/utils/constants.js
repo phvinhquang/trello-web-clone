0324dddedc53aa4a1b36c438f7f4ca5cd534caa2
Jeremy <jeremypisces04@gmail.com>
2024-02-15 15:41:08 +0700

    Add production domain to cors

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -4,4 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [
+  "http://localhost:5173",
+  "https://trello-clone-web.netlify.app",
+];
